5663f3d5029c6d911f57b9d39a8a7c7112c1743b
rohanpv17 <rohanpv2005@gmail.com>
2024-10-22 12:16:01 +0530

    Updated my files

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 // src/pages/_app.tsx
-import "@/styles/globals.css"; // Global Tailwind styles
-import "@/styles/App.css"; // Import custom App.css for additional styles
+import "./styles/globals.css"; // Global Tailwind styles
+import "./styles/App.css"; // Import custom App.css for additional styles
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
